283e40dc75619829437b92ebcaabe6ba4b8f792c
Alexis Peralta <l.pera040904@itses.edu.mx>
2025-03-27 09:38:43 -0600

    Creacion de login

--- cityexplorer/src/app/app.component.ts
+++ cityexplorer/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
+//import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [InicioSesionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ cityexplorer/src/app/resgistro/resgistro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resgistro',
+  imports: [],
+  templateUrl: './resgistro.component.html',
+  styleUrl: './resgistro.component.scss'
+})
+export class ResgistroComponent {
+
+}
