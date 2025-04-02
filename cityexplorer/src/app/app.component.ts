b78e01045d11d1f600bc54d1139ba7bd79d907ee
Alexis Peralta <l.pera040904@itses.edu.mx>
2025-04-02 12:20:28 -0600

    vista home

--- cityexplorer/src/app/app.component.ts
+++ cityexplorer/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
+import { CategoriasComponent } from "./categorias/categorias.component";
+import { HomeComponent } from "./homee/home.component";
 //import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
 
 @Component({
   selector: 'app-root',
-  imports: [InicioSesionComponent],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
